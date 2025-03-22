fa0413658f711fd06c660b0be4d62cddccfb94ea
richardzmija <165653574+richardzmija@users.noreply.github.com>
2025-03-22 14:05:19 +0100

    Initial commit.

---
+++ backend/src/test/java/edu/agh/recipe/RecipeOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.agh.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
