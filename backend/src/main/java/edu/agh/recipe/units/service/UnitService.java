9161339afdaf66459ad5a1cf777f30018daadd58
richardzmija <165653574+richardzmija@users.noreply.github.com>
2025-04-19 20:43:35 +0200

    Refactored UnitService.

--- backend/src/main/java/edu/agh/recipe/units/service/UnitService.java
+++ backend/src/main/java/edu/agh/recipe/units/service/UnitService.java
@@ -1,38 +1,13 @@
 package edu.agh.recipe.units.service;
 
-import edu.agh.recipe.units.domain.MeasurementUnit;
-import edu.agh.recipe.units.domain.MeasurementSystem;
 import edu.agh.recipe.units.dto.UnitDTO;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
 
+public interface UnitService {
 
-@Service
-public class UnitService {
+    List<UnitDTO> getAllUnits();
+    List<UnitDTO> getMetricUnits();
+    List<UnitDTO> getImperialUnits();
 
-    @Cacheable("allUnits")
-    public List<UnitDTO> getAllUnits() {
-        return Arrays.stream(MeasurementUnit.values())
-                .map(UnitDTO::of)
-                .toList();
-    }
-
-    @Cacheable("metricUnits")
-    public List<UnitDTO> getMetricUnits() {
-        return Arrays.stream(MeasurementUnit.values())
-                .filter(unit -> unit.getSystem() == MeasurementSystem.METRIC)
-                .map(UnitDTO::of)
-                .toList();
-    }
-
-    @Cacheable("imperialUnits")
-    public List<UnitDTO> getImperialUnits() {
-        return Arrays.stream(MeasurementUnit.values())
-                .filter(unit -> unit.getSystem() == MeasurementSystem.IMPERIAL)
-                .map(UnitDTO::of)
-                .toList();
-    }
 }
