4773cbd8adb2065f51be7c60409d108f7ef61cfd
richardzmija <165653574+richardzmija@users.noreply.github.com>
2025-04-22 09:56:27 +0200

    Fixed warnings related to serialization of PageImpl.

--- backend/src/main/java/edu/agh/recipe/configuration/WebMvcConfig.java
+++ backend/src/main/java/edu/agh/recipe/configuration/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package edu.agh.recipe.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
