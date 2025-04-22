58e60ca3344eb90ff5618dab4790b7dc9e4905b7
Jan Pawlica <szopen1290@gmail.com>
2025-04-22 19:42:16 +0200

    Changes after code review

---
+++ frontend/src/components/recipe/viewinvidual/RecipeContent.tsx
@@ -0,0 +1,63 @@
+import { VStack, Heading, Text, List, Flex, HStack, Badge, Separator, Box, Image, Card } from '@chakra-ui/react';
+import StepNavigator from './StepNavigator';
+import { Recipe } from '../../../types/Recipe';
+
+interface RecipeContentProps {
+  recipe: Recipe;
+}
+
+export const RecipeContent = ({ recipe }: RecipeContentProps) => (
+  <VStack align='stretch'>
+    <Flex direction={{ base: 'column', md: 'row' }} align='flex-start'>
+      <Box flex='2'>
+        <Heading as='h1' size='3xl' mb={1}>
+          {recipe.name}
+        </Heading>
+
+        {recipe.tags && recipe.tags.length > 0 && (
+          <HStack spaceX={2} mb={4}>
+            {recipe.tags.map((tag, index) => (
+              <Badge key={index} colorPalette='orange' variant='surface' shadow='sm'>
+                {tag}
+              </Badge>
+            ))}
+          </HStack>
+        )}
+
+        {recipe.description && <Text fontSize='md'>{recipe.description}</Text>}
+      </Box>
+      {recipe.image && (
+        <Box flex='1'>
+          <Image src={recipe.image} />
+        </Box>
+      )}
+    </Flex>
+
+    <Separator />
+
+    <Box>
+      <Heading as='h2' size='xl' mb='2'>
+        Ingredients
+      </Heading>
+      <Card.Root variant='outline' shadow='xl'>
+        <Card.Body>
+          <List.Root>
+            {recipe.ingredients.map((ingredient, index) => (
+              <List.Item key={index}>
+                {ingredient.ingredientName} {ingredient.quantity} {ingredient.metric.toLowerCase()}
+              </List.Item>
+            ))}
+          </List.Root>
+        </Card.Body>
+      </Card.Root>
+    </Box>
+
+    <Heading as='h2' size='xl'>
+      Steps
+    </Heading>
+
+    <StepNavigator steps={recipe.steps} />
+  </VStack>
+);
+
+export default RecipeContent;
